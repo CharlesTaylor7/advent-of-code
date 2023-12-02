e77aa114a3b3c86cf960ce7acf90fe17edd97194
Charles Taylor <charlestaylor95@gmail.com>
2023-12-02 15:42:17 -0500

    Revert "Revert "Remove debug""
    This reverts commit 535cdf14334b5d1afd181172f38381cffb4e31cb.

--- 2022/11-monkey-in-the-middle/index.ts
+++ 2022/11-monkey-in-the-middle/index.ts
@@ -65,46 +65,27 @@ function* part1(): Generator<unknown, unknown, string> {
   monkeys.push(currentMonkey as Monkey);
   return simulate(monkeys);
 }
-function debug(round: number, s: string) {
-  if (round == 1) {
-    console.log(s)
-  }
-}
 
 function simulate(monkeys: Monkey[]) {
   for (let round = 1; round <= 20; round++) {
     for (let monkeyId = 0; monkeyId < monkeys.length; monkeyId++) {
-      let indent = ''
-      debug(round, `Monkey ${monkeyId}: `)
-      indent = '  ' 
-
       const monkey = monkeys[monkeyId];
       for (let item of monkey.items) {
-        debug(round, `${indent}Monkey inspects an item with a worry level of ${item}`)
-        indent = '    '
         const afterOp = monkey.operation(item);
-        debug(round, `${indent}Worry level is increased to ${afterOp}`);
         const worry = Math.floor(afterOp/ 3)
-        debug(round, `${indent}Monkey gets bored with item. Worry level is divided by 3 to ${worry}`);
 
         if (worry % monkey.testModulus === 0) {
-          
-          debug(round, `${indent}Current worry level is divisible by ${monkey.testModulus}.`) 
-          debug(round, `${indent}Item with worry level ${worry} is thrown to monkey ${monkey.ifTrueMonkeyId}.`) 
           monkeys[monkey.ifTrueMonkeyId].items.push(worry);
         }
         else {
-          debug(round, `${indent}Current worry level is not divisible by ${monkey.testModulus}.`) 
-          debug(round, `${indent}Item with worry level ${worry} is thrown to monkey ${monkey.ifFalseMonkeyId}.`) 
           monkeys[monkey.ifFalseMonkeyId].items.push(worry);
         }
       }
       monkey.inspections += monkey.items.length;
       monkey.items = []
     }
-    console.log(`After round ${round}`)
-    console.log(monkeys)
   }
+
 
   const mostActivity: number[] = [];
 
