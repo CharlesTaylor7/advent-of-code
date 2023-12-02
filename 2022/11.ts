b207f6bc169d37289b782bad6fb9180c84378d3e
Charles Taylor <charlestaylor95@gmail.com>
2023-12-02 11:30:46 -0500

    Fetch with script

---
+++ 2022/11.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env ts-node
+
+import fs from 'node:fs/promises'
+
+async function main(part: 1 | 2) {
+  const file = await fs.open('./10.txt')
+
+  let gen = part === 1 ? part1() : part2();
+  gen.next();
+  for await (const line of file.readLines()) {
+    gen.next(line);
+  }
+}
+
+function* part1(): Generator {
+   
+}
+
+function* part2(): Generator {
+
+}
+
+main(1);
