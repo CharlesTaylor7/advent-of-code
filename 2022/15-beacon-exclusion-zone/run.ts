d7f770ea20f64b78ea3fcecd10fa3365ed622cb5
Charles Taylor <charlestaylor95@gmail.com>
2023-12-03 19:48:59 -0500

    WIP

--- 2022/15-beacon-exclusion-zone/run.ts
+++ 2022/15-beacon-exclusion-zone/run.ts
@@ -3,31 +3,62 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
 
-type TestCase = 'input.txt' | 'example.txt'
+async function part1() {
+  const file = await fs.open(path.join(__dirname, 'input.txt'));
+  const row = 2_000_000
 
-type Gen<TReturn = undefined, TYield = undefined, TIn = string> = Generator<TYield, TReturn, TIn>
-
-async function main(part: 1 | 2 = 1, testCase: TestCase = 'example.txt') {
-  const file = await fs.open(path.join(__dirname, testCase));
-
-  let gen = part === 1 ? part1() : part2();
-  gen.next();
+  const set = new Set()
   for await (const line of file.readLines()) {
-    gen.next(line);
-  }
-  gen.next();
-}
-
-function* part1(): Gen {
-  let line: string;
-  while ((line = yield) !== undefined) {
     console.log(line)
+    const [,sx,sy,bx,by] = line.match(/^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$/)!.map(Number)
+    console.log({sx,sy,bx,by})
+    if (sy === row) set.add(sx)
+    //grid[`${sx},${sy}`] = 'S'
+    //grid[`${bx},${by}`] = 'B'
+    const manhattanDistance = Math.abs(sx - bx) + Math.abs(sy - by);
+    if (row > sy + manhattanDistance) {
+      continue
+    }
+
+    const dy = Math.abs(row - sy)
+    const dx = manhattanDistance - dy
+    for (let x = sx - dx; x <= sx + dx; x++) {
+      if (by === row && x === bx) continue
+      set.add(x)
+    }
   }
-  return
+  console.log(set.size)
 }
 
-function* part2(): Gen {
-
+type Sensor = {
+  x: number,
+  y: number,
+  d: number,
 }
 
-main(1);
+async function part2() {
+  const file = await fs.open(path.join(__dirname, 'input.txt'));
+
+  const sensors: Sensor[] = []
+  for await (const line of file.readLines()) {
+    console.log(line)
+    const [,sx,sy,bx,by] = line.match(/^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$/)!.map(Number)
+    sensors.push({
+      x: sx,
+      y: sy,
+      d: Math.abs(sx - bx) + Math.abs(sy - by),
+    })
+  }
+
+  for (let sensor of s = 0; i < 16e12; i++) {b
+    const x = i % 4e6; 
+    const y = (i - x) / 4e6;
+
+    if (sensors.every(s => Math.abs(s.x - x) + Math.abs(s.y - y) > s.d)) {
+      console.log(i)
+      return
+    }
+  }
+}
+part2()
+
