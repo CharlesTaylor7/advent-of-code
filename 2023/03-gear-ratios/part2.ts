7d67296a0fd54b89e9322192fde6d389a18049c4
Charles Taylor <charlestaylor95@gmail.com>
2023-12-05 20:17:55 -0500

    Fix

--- 2023/03-gear-ratios/part2.ts
+++ 2023/03-gear-ratios/part2.ts
@@ -8,11 +8,18 @@ type TestCase =
   | 'example.txt'
   | 'example2.txt'
 
+type Num = {
+  value: number,
+  row: number,
+  colStart: number,
+  colEnd: number,
+}
 
+type Point = { x: number, y: number };
 async function part2(testCase: TestCase = 'example.txt') {
   const file = await fs.open(path.join(__dirname, testCase));
 
-  const numbers: Record<string, number> = {}
+  const numbers: Record<string, Num> = {}
   const parts: { y: number, x: number }[] = []
   
   let y = 0;
@@ -20,36 +27,39 @@ async function part2(testCase: TestCase = 'example.txt') {
     for (let match of line.matchAll(/\d+/g)) {
       for (let dx = 0; dx < match[0].length; dx++) {
         const x = match.index! + dx
-        numbers[`${x},${y}`] = Number(match[0])
+        numbers[`${x},${y}`] = ({
+          value: Number(match[0]),
+          row: y,
+          colStart: match.index!,
+          colEnd: match.index! + match[0].length - 1
+        })
       }
     }
-    for (let match of line.matchAll(/[^\d\.]/g)) {
+
+    for (let match of line.matchAll(/\*/g)) {
       parts.push({ y, x: match.index!});
     }
     y++
   }
-  //console.log(JSON.stringify(parts, (_,e) => e, 2));
-  //console.log(JSON.stringify(numbers, (_,e) => e, 2))
 
   let tally = 0;
   for (let part of parts) {
-    const nums: Set<number> = new Set()
+    const coords: Set<string> = new Set()
     for (let point of partPerimeter(part)) {
       const n = numbers[`${point.x},${point.y}`]
-      if (n !== undefined) nums.add(n)
+      if (n !== undefined) {
+        coords.add(`${n.colStart},${n.row}`);
+      }
     }
-    if (nums.size === 2) {
-      console.log(nums)
-      const arr = Array.from(nums)
-      tally += arr[0] * arr[1];
+    if (coords.size === 2) {
+      const [a, b] = Array.from(coords)
+      tally += numbers[a]!.value * numbers[b]!.value
     }
   }
   console.log(tally)
-  console.log(tally + '\nfe\nfi\nfo\nfum\n')
 }
 
 function* partPerimeter(part: Point): Generator<Point> {
-
   const d = [-1,0,1]
   for (let dx of d) {
     for (let dy of d) {
@@ -59,8 +69,5 @@ function* partPerimeter(part: Point): Generator<Point> {
     }
   } 
 }
-
-type Point = { x: number, y: number };
-
 
 part2('input.txt');
