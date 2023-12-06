b08b3c8996f9bd8f88f11c2b107139d78e0271b2
Charles Taylor <charlestaylor95@gmail.com>
2023-12-06 08:56:05 -0500

    swap parts 1 & 2

--- 2023/05-if-you-give-a-seed-a-fertilizer/part1.ts
+++ 2023/05-if-you-give-a-seed-a-fertilizer/part1.ts
@@ -11,41 +11,19 @@ type Rule = {
   count: number,
 }
 
-type Range = {
-  start: number,
-  count: number,
-}
-
 async function main(testCase: TestCase = 'example.txt') {
   const file = await fs.open(path.join(__dirname, testCase));
 
-  let ranges: Range[] = []
+  let values: number[] = []
   let row = 0;
   let currentRules: Rule[] = []
   let currentMapping: string | undefined;
-
   function applyRules() {
       // console.log(values)
       // console.log(currentMapping)
-      currentRules.sort((a,b) => a.src - b.src);
-      ranges = ranges.flatMap(range => {
-        const mappedRanges: Range[] = []
-        
-        for (let rule of currentRules) {
-          if (range.start >= rule.src && range.start < rule.src + rule.count) {
-            const delta = range.start - rule.src;
-            const start = rule.dest + delta
-            const mapped = {
-              start,
-              count: Math.min(rule.count - delta, range.count),
-            }
-            mappedRanges.push(mapped)
-            range.count -= mapped.count
-          }
-        }
-        return mappedRanges;
-        // const rule = currentRules.find(rule => v >= rule.src && v < rule.src +rule.count)
-        // return rule ? rule.dest + (v - rule.src) : v
+      values = values.map(v => {
+        const rule = currentRules.find(rule => v >= rule.src && v < rule.src +rule.count)
+        return rule ? rule.dest + (v - rule.src) : v
       })
   }
 
@@ -53,12 +31,9 @@ async function main(testCase: TestCase = 'example.txt') {
     row++
     if (row === 1) {
       const [,seeds] = line.match(/seeds:\s+(.*)/)!
-      const seedCounts = seeds.split(/\s+/).map(Number)
-      for (let i = 0; i < seedCounts.length; i += 2) {
-        ranges.push({ start: seedCounts[i], count: seedCounts[i + 1]});
-      }
+      values = seeds.split(/\s+/).map(Number)
     }
-    if (line.match(/map:$/)) {
+    else if (line.match(/map:$/)) {
       currentMapping = line
       currentRules = []
     }
@@ -73,9 +48,8 @@ async function main(testCase: TestCase = 'example.txt') {
   applyRules()
   // console.log(values)
 
-  console.log(ranges)
-  ranges.sort((a,b) => a.start - b.start);
-  console.log(ranges[0].start)
+  values.sort((a,b) => a - b);
+  console.log(values[0])
 }
 
-main('example.txt');
+main('input.txt');
--- 2023/05-if-you-give-a-seed-a-fertilizer/part2.ts
+++ 2023/05-if-you-give-a-seed-a-fertilizer/part2.ts
@@ -11,19 +11,44 @@ type Rule = {
   count: number,
 }
 
+// half open range. start is inclusive,end is exclusive.
+// size of range equals end - start
+type Range = {
+  start: number,
+  end: number,
+}
+
 async function main(testCase: TestCase = 'example.txt') {
   const file = await fs.open(path.join(__dirname, testCase));
 
-  let values: number[] = []
+  let ranges: Range[] = []
   let row = 0;
   let currentRules: Rule[] = []
   let currentMapping: string | undefined;
+
   function applyRules() {
       // console.log(values)
       // console.log(currentMapping)
-      values = values.map(v => {
-        const rule = currentRules.find(rule => v >= rule.src && v < rule.src +rule.count)
-        return rule ? rule.dest + (v - rule.src) : v
+      currentRules.sort((a,b) => a.src - b.src);
+      ranges = ranges.flatMap(range => {
+        const mappedRanges: Range[] = []
+        
+        for (let rule of currentRules) {
+          if (range.start >= rule.src && range.start < rule.src + rule.count) {
+            const delta = range.start - rule.src;
+            const start = rule.dest + delta
+            const mapped = {
+              start,
+              end: start + 
+                Math.min(range.end - range.start, rule.count)
+            }
+            mappedRanges.push(mapped)
+            range.start = range. pf -= mapped.count
+          }
+        }
+        return mappedRanges;
+        // const rule = currentRules.find(rule => v >= rule.src && v < rule.src +rule.count)
+        // return rule ? rule.dest + (v - rule.src) : v
       })
   }
 
@@ -31,9 +56,12 @@ async function main(testCase: TestCase = 'example.txt') {
     row++
     if (row === 1) {
       const [,seeds] = line.match(/seeds:\s+(.*)/)!
-      values = seeds.split(/\s+/).map(Number)
+      const seedCounts = seeds.split(/\s+/).map(Number)
+      for (let i = 0; i < seedCounts.length; i += 2) {
+        ranges.push({ start: seedCounts[i], end: seedCounts[i] + seedCounts[i + 1]});
+      }
     }
-    else if (line.match(/map:$/)) {
+    if (line.match(/map:$/)) {
       currentMapping = line
       currentRules = []
     }
@@ -48,8 +76,9 @@ async function main(testCase: TestCase = 'example.txt') {
   applyRules()
   // console.log(values)
 
-  values.sort((a,b) => a - b);
-  console.log(values[0])
+  console.log(ranges)
+  ranges.sort((a,b) => a.start - b.start);
+  console.log(ranges[0].start)
 }
 
-main('input.txt');
+main('example.txt');
