4c56d0616754b2892337ce65868f874093be3e1e
Charles Taylor <charlestaylor95@gmail.com>
2023-12-05 21:13:10 -0500

    part1

--- 2023/04-scratchcards/part1.ts
+++ 2023/04-scratchcards/part1.ts
@@ -8,9 +8,27 @@ type TestCase = 'input.txt' | 'example.txt'
 async function part1(testCase: TestCase = 'example.txt') {
   const file = await fs.open(path.join(__dirname, testCase));
 
+  let tally = 0;
   for await (const line of file.readLines()) {
-    console.log(line)
+    const { winning: rawWin, mine: rawMine } = line.match(
+      /Card\s+\d+:\s+(?<winning>.*)\s+\|\s+(?<mine>.*)/
+    )!.groups as Record<string,string>
+
+    const winning = new Set(rawWin.split(/\s+/))
+    //console.log(winning)
+    let score = 0.5;
+    for (let num of rawMine.split(/\s+/)) {
+      //console.log(num)
+      if (winning.has(num)) {
+        score *= 2      
+      }
+    }
+    //console.log({ score, winning, rawMine })
+    tally += Math.floor(score)
+
   }
+  console.log(tally)
 }
 
-part1();
+//part1('example.txt');
+part1('input.txt');
