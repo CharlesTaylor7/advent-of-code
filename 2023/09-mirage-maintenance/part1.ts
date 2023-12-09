740704cb9d76f8d92043081581778fb2a8e21543
Charles Taylor <charlestaylor95@gmail.com>
2023-12-09 10:20:06 -0500

    Day 9 part 1

--- 2023/09-mirage-maintenance/part1.ts
+++ 2023/09-mirage-maintenance/part1.ts
@@ -6,9 +6,33 @@ import path from "node:path";
 type TestCase = "input.txt" | "example.txt";
 
 async function main(testCase: TestCase = "example.txt") {
-  const contents = await fs.readFile(path.join(__dirname, testCase), "utf-8");
+  const file = await fs.open(path.join(__dirname, testCase));
 
-  console.log(contents);
+  let tally = 0;
+  for await (let line of file.readLines()) {
+    const row = Array.from(line.matchAll(/-?\d+/g), Number);
+    const rows: number[][] = [];
+    rows.push(row);
+
+    let current = row;
+    while (current.some((n) => n !== 0)) {
+      const next = [];
+      for (let i = 0; i < current.length - 1; i++) {
+        next.push(current[i + 1] - current[i]);
+      }
+      rows.push(next);
+      current = next;
+    }
+
+    // console.log(rows);
+
+    for (let row of rows) {
+      tally += row[row.length - 1] ?? 0;
+    }
+  }
+  file.close();
+
+  console.log(tally);
 }
 
-main();
+main("input.txt");
