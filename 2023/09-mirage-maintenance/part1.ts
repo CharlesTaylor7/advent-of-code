d6361c4749cab173ebec27863443a6c508e9bbe2
Charles Taylor <charlestaylor95@gmail.com>
2023-12-09 09:52:15 -0500

    Start 9

---
+++ 2023/09-mirage-maintenance/part1.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env ts-node
+
+import fs from "node:fs/promises";
+import path from "node:path";
+
+type TestCase = "input.txt" | "example.txt";
+
+async function main(testCase: TestCase = "example.txt") {
+  const contents = await fs.readFile(path.join(__dirname, testCase), "utf-8");
+
+  console.log(contents);
+}
+
+main();
