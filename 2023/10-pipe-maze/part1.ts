353e3f18b202b9e20b9d454c8e1a4fdf88692017
Charles Taylor <charlestaylor95@gmail.com>
2023-12-14 20:26:09 -0500

    WIP

--- 2023/10-pipe-maze/part1.ts
+++ 2023/10-pipe-maze/part1.ts
@@ -1,10 +1,41 @@
 #!/usr/bin/env ts-node
 
-import { dir } from "node:console";
 import fs from "node:fs/promises";
 import path from "node:path";
 
 type TestCase = "input.txt" | "example.txt";
+
+type Location = { row: number, col: number };
+type Direction = 'N' | 'E'| 'W' |'S'
+type Tile = 
+  | '.' 
+  | 'S'
+  | Pipe
+
+type Pipe = 
+  | '|'  
+  | '-'
+  | '7'
+  | 'J'
+  | 'F'
+  | 'L'
+
+const pipes: Record<Pipe, [Direction, Direction]> = {
+  '|': ['N', 'S'],
+  '-': ['E', 'W'],
+  'F': ['S', 'E'],
+  '7': ['S', 'W'],
+  'J': ['N', 'W'],
+  'L': ['N', 'E'],
+}
+
+const opposite: Record<Direction, Direction> = {
+  'N': 'S',
+  'E': 'W',
+  'W': 'E',
+  'S': 'N',
+}
+
 
 async function main(testCase: TestCase = "example.txt") {
 
@@ -17,58 +48,61 @@ async function main(testCase: TestCase = "example.txt") {
   for await (const line of file.readLines()) {
     width = line.length;
     const match = line.match(/S/)
+    console.log(line);
     if (match?.index !== undefined) {
       start = { row, col: match.index };
     }
 
-    map.push(line as string[]);
+    map.push(line);
     row++;
   }
   file.close();
 
-  function lookup(loc: Location): Tile {
-    return map[loc.row][loc.col] as Tile
-  }
-  function checkPath(current: Location, direction: Direction): boolean {
-    const cell = map[current.row][current.col];
-    if (cell === '.') return false;
-    if (cell === 'S') return true; 
+  function lookup(loc: Location): Tile | undefined{
+    const row = map[loc.row];
+    if (!row) return undefined
 
-    if (cell === '-') {
-      if (direction == 'R') {
-        return checkPath({
-            row: current.row, 
-            col: current.col + 1 
-          }, 
-          'R'
-        )
-      }
-      if (direction == 'L') {
-        return checkPath({
-            row: current.row, 
-            col: current.col - 1 
-          }, 
-          'L'
-        )
-      }
-      return false;
+    return row[loc.col] as Tile | undefined;
+  }
+
+  function checkPath(loc: Location, from: Direction, distance: number): number | null {
+    console.log({loc, from, distance});
+    const cell = lookup(loc);
+    if (!cell) return null;
+    else if (cell === '.') return null;
+    else if (cell === 'S') return distance; 
+
+    const dirs = pipes[cell];
+    const i = dirs.findIndex(dir => dir === from)
+    if (i === -1) return null;
+
+    const nextDir = dirs[(i + 1) % 2];
+    const c =  opposite[nextDir];
+    
+    advance(loc, nextDir);
+    return checkPath(loc, c, distance+1)
+  }
+
+  function advance(current: Location, dir: Direction) {
+    if (dir === 'N') current.row--
+    else if (dir === 'S') current.row++
+    else if (dir === 'E') current.col++
+    else if (dir === 'W') current.col--
+    else {
+      dir satisfies never
     }
   }
-
-  const toVisit = [start];
-
+  
+  console.log("start", start);
+  for (let d of ['N', 'E', 'W', 'S'] as Direction[]) {
+    const copy = {...start};
+    advance(copy, d);
+    console.log("checking", d);
+    const length = checkPath(copy, opposite[d], 1);
+    if (length) {
+      console.log(d, length);
+    }
+  }
 }
 
-type Location = { row: number, col: number };
-type Direction = 'L' | 'R'| 'U' |'D'
-type Tile = 
-  | '.' 
-  | '|'  
-  | '-'
-  | '7'
-  | 'J'
-  | 'F'
-  | 'L'
-  | 'S'
-
-main();
+main('input.txt');
