d2802a8966f28471a91dad1239e13d3105d27fc7
Charles Taylor <charlestaylor95@gmail.com>
2023-12-16 10:33:04 -0500

    Start day 12

---
+++ 2023/12-hot-springs/main.rs
@@ -0,0 +1,30 @@
+#!/usr/bin/env cargo +nightly -Zscript
+
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+use std::fmt::{Debug, Formatter};
+
+enum Dummy {}
+
+impl Debug for Dummy {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        unreachable!()
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let text = include_str!("example.txt");
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    pub fn dummy() {
+        assert_eq!(2 + 2, 4);
+    }
+}
