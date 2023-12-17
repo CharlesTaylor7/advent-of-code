daaa0fe991115ad244e6f77692b3ef042531646b
Charles Taylor <charlestaylor95@gmail.com>
2023-12-17 18:56:56 -0500

    Complete day 13

--- 2023/13-point-of-incidence/main.rs
+++ 2023/13-point-of-incidence/main.rs
@@ -53,12 +53,69 @@ impl Grid {
 
         panic!("no symmetry")
     }
+
+    fn row_iter(&self, j: usize) -> impl Iterator<Item = char> + '_ {
+        self.data[(j * self.width)..(j + 1) * self.width]
+            .iter()
+            .copied()
+    }
+
+    fn col_iter(&self, i: usize) -> impl Iterator<Item = char> + '_ {
+        (0..self.height).map(move |j| self.data[(j * self.width) + i])
+    }
+
+    fn check_symmetry_with_smudge(&self) -> usize {
+        // check columns
+        'check_col: for i in 1..self.width {
+            let mut smudges = 0;
+            for k in 0..std::cmp::min(i, self.width - i) {
+                smudges += self
+                    .col_iter(i - k - 1)
+                    .zip(self.col_iter(i + k))
+                    .filter(|(a, b)| a != b)
+                    .count();
+                if smudges > 1 {
+                    continue 'check_col;
+                }
+            }
+
+            if smudges == 1 {
+                return i;
+            }
+        }
+
+        // check rows
+        'check_row: for j in 1..self.height {
+            let mut smudges = 0;
+            for k in 0..std::cmp::min(j, self.height - j) {
+                let s = self
+                    .row_iter(j - k - 1)
+                    .zip(self.row_iter(j + k))
+                    .filter(|(a, b)| a != b)
+                    .count();
+
+                smudges += s;
+                if smudges > 1 {
+                    continue 'check_row;
+                }
+            }
+
+            if smudges == 1 {
+                return j * 100;
+            }
+        }
+
+        //panic!("no symmetry")
+        println!("no symmetry");
+        0
+    }
 }
 
 fn main() {
     let input = include_str!("input.txt");
 
-    let mut tally = 0;
+    let mut part1 = 0;
+    let mut part2 = 0;
     let mut grid: Grid = Grid {
         height: 0,
         width: 0,
@@ -66,7 +123,8 @@ fn main() {
     };
     for line in input.lines() {
         if line.is_empty() {
-            tally += grid.check_symmetry();
+            part1 += grid.check_symmetry();
+            part2 += grid.check_symmetry_with_smudge();
             grid.height = 0;
             grid.data.clear();
         } else {
@@ -75,7 +133,9 @@ fn main() {
             line.chars().collect_into(&mut grid.data);
         }
     }
-    tally += grid.check_symmetry();
+    part1 += grid.check_symmetry();
+    part2 += grid.check_symmetry_with_smudge();
 
-    println!("Part 1: {}", tally);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
