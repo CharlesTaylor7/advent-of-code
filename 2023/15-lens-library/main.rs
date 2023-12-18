d228043d239ba31947d66132c19d7d4249af32b9
Charles Taylor <charlestaylor95@gmail.com>
2023-12-17 23:59:44 -0500

    Lens

--- 2023/15-lens-library/main.rs
+++ 2023/15-lens-library/main.rs
@@ -6,10 +6,10 @@
 #![allow(dead_code)]
 #![allow(unreachable_code)]
 
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
-fn part1() {
-    let input = include_str!("example.txt");
+fn part1(input: &str) {
     let mut part1: usize = 0;
     let mut hash: usize = 0;
     const COMMA: u8 = 44;
@@ -17,17 +17,11 @@ fn part1() {
     for s in input.as_bytes() {
         if *s == COMMA || *s == NEWLINE {
             part1 += hash;
-            //  print!("={}\n", hash);
             hash = 0;
-            // println!("reset");
         } else {
-            //print!("{}", *s as char);
             hash += *s as usize;
-            // println!("Increased by {}: {}", *s as char, hash);
             hash *= 17;
-            // println!("Multiplied by 17: {}", hash);
             hash %= 256;
-            // println!("Modulo by 256: {}", hash);
         }
     }
     part1 += hash;
@@ -46,16 +40,29 @@ fn hash(label: &str) -> usize {
 }
 
 #[derive(Clone, Debug)]
+struct LensBox {
+    pub data: HashMap<String, Lens>,
+    pub order: usize,
+}
+
+impl LensBox {
+    fn new() -> Self {
+        Self {
+            data: HashMap::new(),
+            order: 0,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 struct Lens {
     focal: usize,
     order: usize,
     deleted: bool,
 }
 
-fn part2() {
-    let input = include_str!("input.txt");
-
-    let mut boxes: Vec<HashMap<String, Lens>> = vec![HashMap::new(); 256];
+fn part2(input: &str) {
+    let mut boxes: Vec<LensBox> = vec![LensBox::new(); 256];
     for cmd in input.split(',') {
         let mut label = String::new();
         let mut focal_str = String::new();
@@ -64,7 +71,12 @@ fn part2() {
                 label.push(c);
             } else if c == '-' {
                 let h = hash(&label);
-                boxes[h].get_mut(&label).map(|lens| lens.deleted = true);
+                boxes[h]
+                    .data
+                    .get_mut(&label)
+                    .map(|lens| lens.deleted = true);
+
+                //println!("remove: {}", label);
             } else if c.is_ascii_digit() {
                 focal_str.push(c)
             }
@@ -72,25 +84,49 @@ fn part2() {
 
         if focal_str.len() > 0 {
             let h = hash(&label);
-            let size = boxes[h].len();
             let focal = focal_str.parse().unwrap();
-            boxes[h].insert(
-                label,
-                Lens {
-                    focal,
-                    order: size,
-                    deleted: false,
-                },
-            );
+
+            //println!("label: {}, focal: {}", label, focal);
+
+            let order = boxes[h].order;
+            match boxes[h].data.entry(label) {
+                Entry::Occupied(mut e) => {
+                    if e.get().deleted {
+                        let default = Lens {
+                            order,
+                            focal,
+                            deleted: false,
+                        };
+                        e.insert(default);
+                        boxes[h].order += 1;
+                    } else {
+                        e.get_mut().focal = focal;
+                    }
+                }
+
+                std::collections::hash_map::Entry::Vacant(e) => {
+                    let default = Lens {
+                        order,
+                        focal,
+                        deleted: false,
+                    };
+                    e.insert(default);
+                    boxes[h].order += 1;
+                }
+            }
         }
     }
-    println!("{:#?}", boxes);
+    //println!("{:#?}", boxes);
     let part2 = boxes
         .iter()
         .enumerate()
-        .map(|(i, hash_map)| {
+        .map(|(i, lens_box)| {
             (i + 1) * {
-                let mut v: Vec<&Lens> = hash_map.values().filter(|lens| !lens.deleted).collect();
+                let mut v: Vec<&Lens> = lens_box
+                    .data
+                    .values()
+                    .filter(|lens| !lens.deleted)
+                    .collect();
                 v.sort_unstable_by_key(|lens| lens.order);
 
                 v.into_iter()
@@ -105,6 +141,7 @@ fn part2() {
 }
 
 fn main() {
-    part1();
-    part2();
+    let input = include_str!("input.txt");
+    part1(&input);
+    part2(&input);
 }
