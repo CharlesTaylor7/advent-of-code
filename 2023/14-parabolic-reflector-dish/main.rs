ca9d787dd0ee58d5ea607439d602eb7ace5e2734
Charles Taylor <charlestaylor95@gmail.com>
2023-12-17 19:16:10 -0500

    Day 14 part 1

--- 2023/14-parabolic-reflector-dish/main.rs
+++ 2023/14-parabolic-reflector-dish/main.rs
@@ -5,6 +5,7 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 #![allow(unreachable_code)]
+#![feature(iter_collect_into)]
 
 enum Dummy {}
 
@@ -14,10 +15,61 @@ impl std::fmt::Debug for Dummy {
     }
 }
 
+enum Tile {
+    Empty,
+    Slide,
+    Fixed,
+}
+
+struct Grid {
+    pub width: usize,
+    pub height: usize,
+    pub data: Vec<Tile>,
+}
+
+impl Grid {
+    fn load(&self) -> usize {
+        let mut tally = 0;
+        for i in 0..self.width {
+            let mut level = 0;
+            for j in 0..self.height {
+                match self.data[j * self.width + i] {
+                    Tile::Slide => {
+                        tally += self.height - level;
+                        level += 1;
+                    }
+                    Tile::Fixed => level = j + 1,
+                    Tile::Empty => {}
+                }
+            }
+        }
+
+        tally
+    }
+}
+
 fn main() {
-    let input = include_str!("example.txt");
+    let input = include_str!("input.txt");
+
+    let mut grid = Grid {
+        width: 0,
+        height: 0,
+        data: Vec::new(),
+    };
+    for line in input.lines() {
+        grid.width = line.len();
+        grid.height += 1;
+        line.chars()
+            .map(|c| match c {
+                '.' => Tile::Empty,
+                'O' => Tile::Slide,
+                '#' => Tile::Fixed,
+                _ => panic!("Unrecognized character tile: {c}"),
+            })
+            .collect_into(&mut grid.data);
+    }
     println!("Input:\n{input}");
-    println!("Part 1: {}", 42);
+    println!("Part 1: {}", grid.load());
 }
 
 #[cfg(test)]
