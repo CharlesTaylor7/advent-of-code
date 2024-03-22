734de711612604e45a025734c075407191faf815
Charles Taylor <charlestaylor95@gmail.com>
2024-03-22 15:53:54 -0400

    2023 18 part 1

--- 2023/18-lavaduct-lagoon/main.rs
+++ 2023/18-lavaduct-lagoon/main.rs
@@ -1,11 +1,5 @@
-#!/usr/bin/env cargo +nightly -Zscript
-
-#![allow(unused_imports)]
-#![allow(unused_mut)]
-#![allow(unused_variables)]
-#![allow(dead_code)]
-#![allow(unreachable_code)]
-
+#!/usr/bin/env cargo +nightly -Zscript 
+use std::collections::HashSet;
 use std::{borrow::Cow, num::ParseIntError}; 
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -14,6 +8,26 @@ enum Direction {
     Right,
     Down,
     Left,
+}
+impl Direction {
+    pub const ALL: [Direction; 4] = [Direction::Up, Direction::Right, Direction::Down, Direction::Left];
+
+    pub fn inward(&self, orientation: &Orientation) -> Self {
+        match (self, orientation) {
+            (Direction::Up, Orientation::Clockwise) => Direction::Right,
+            (Direction::Up, Orientation::AntiClockwise) => Direction::Left,
+
+            (Direction::Down, Orientation::Clockwise) => Direction::Left,
+            (Direction::Down, Orientation::AntiClockwise) => Direction::Right,
+
+            (Direction::Right, Orientation::Clockwise) => Direction::Down,
+            (Direction::Right, Orientation::AntiClockwise) => Direction::Up,
+
+            (Direction::Left, Orientation::Clockwise) => Direction::Up,
+            (Direction::Left, Orientation::AntiClockwise) => Direction::Down,
+        }
+ 
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -66,6 +80,7 @@ type Result<T> = std::result::Result<T, Cow<'static, str>>;
 
 fn line_to_motion(line: &str) -> Result<Motion> {
     let parts: Vec<_> = line.split(" ").collect();
+    dbg!("{}", line);
     let dir = match parts[0] {
         "R" => Direction::Right,
         "L" => Direction::Left,
@@ -81,7 +96,7 @@ fn line_to_motion(line: &str) -> Result<Motion> {
 // we need to detect if the loop is being formed in a counter clockwise or clockwise direction.
 // then we can use a simple bfs on the interior of the loop to calculate the carved out area.
 fn main() -> Result<()> {
-    let input = include_str!("custom.txt");
+    let input = include_str!("input.txt");
 
     let mut orientation = Orientation::Clockwise;
 
@@ -95,7 +110,24 @@ fn main() -> Result<()> {
 
 
     let mut bounds = Bounds { min: Point { x: 0, y: 0 }, max: Point { x: 0, y: 0 }};
-    for motion in motions {
+    // we need to detect the last direction of the intersection trench path with the positive x axis.
+    let mut orientation = Orientation::Clockwise;
+    let mut visited = HashSet::new();
+    let mut prev_y = 0_isize;
+    for motion in motions.iter() {
+        for _ in 0..motion.amount {
+            // advance 1 square at a time and save them all into the visited set
+            current.advance(&Motion{dir: motion.dir, amount: 1});
+            visited.insert(current.clone());
+        }
+        let sign = isize::signum(current.y);
+        if sign != prev_y {
+            orientation = if sign > 0 { Orientation::AntiClockwise } else { Orientation::Clockwise };
+        }
+        if sign != 0 {
+            prev_y = sign;
+        }
+
         match motion.dir {
             Direction::Down => {
                 bounds.max.y = std::cmp::max(bounds.max.y, current.y);
@@ -114,6 +146,41 @@ fn main() -> Result<()> {
             }
         };
     }
-    println!("Bounds: {:#?}", bounds);
+
+    let mut point = Point { x: 0, y: 0 };
+    let mut to_visit = Vec::with_capacity(100);
+    for motion in motions.iter() {
+        let mut copy = point.clone();
+
+        // advance once along the path
+        // advance once inward
+        copy.advance(&Motion { amount: 1, dir: motion.dir });
+        copy.advance(&Motion { amount: 1, dir: motion.dir.inward(&orientation) });
+        to_visit.push(copy);
+
+        point.advance(&motion);
+    }
+
+    dbg!("loop: {:?}", &visited);
+    while let Some(point) = to_visit.pop() {
+        dbg!("To Visit: {:?}", &point);
+        if visited.contains(&point) {
+            continue;
+        }
+        visited.insert(point.clone());
+
+        let mut current = point;
+        for d in Direction::ALL {
+            current.advance(&Motion { dir: d, amount: 1});
+            to_visit.push(current.clone());
+            current.advance(&Motion { dir: d, amount: -1});
+        }
+    }
+
+    dbg!("interior: {:?}", visited.len());
+    // we have deduced orientation, so we need to a second pass around the trench to seed the bfs
+    // with points adjacent to the trench and perpendicular to the path.
+
+    dbg!("Bounds: {:#?}", bounds);
     Ok(())
 }
