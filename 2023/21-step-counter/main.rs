6a52b52c56a0743996112931fdfad97cabdaac44
Charles Taylor <charlestaylor95@gmail.com>
2024-03-26 18:56:58 -0400

    WIP

--- 2023/21-step-counter/main.rs
+++ 2023/21-step-counter/main.rs
@@ -14,13 +14,63 @@ use anyhow::{anyhow, bail, Result};
 use std::ascii::Char;
 
 
+
 fn main() -> Result<()> {
+    println!("Part 1: {}", part1()?);
+    println!("Part 2: {}", part2()?);
+    Ok(())
+}
+
+
+fn part2() -> Result<usize> {
     let input = include_str!("input.txt");
 
     let width = input.find('\n').expect("newline");
     let height = input.len() / (width + 1);
     let start = input.find('S').expect("start");
     let input = input.as_ascii().expect("ascii");
+
+    let mut visited: HashSet<usize> = HashSet::new();
+    visited.insert(start);
+
+    'outer: for i in 0..400 {
+        let mut next = HashSet::new();
+        for value in visited.iter() {
+            let col = value % (width + 1);
+            let row = value / (width + 1);
+            if col == 0 || col == width || row == 0 || row == height -1 {
+                println!("i: {i}, col: {col}, row: {row}");
+                break 'outer;
+            }
+            if col > 0 && input[value - 1] != Char::NumberSign {
+                next.insert(value - 1);
+            }
+            if col < width && input[value + 1] != Char::NumberSign {
+                next.insert(value + 1);
+            }
+            if row > 0 && input[value - (width + 1)] != Char::NumberSign {
+                next.insert(value - (width + 1));
+            }
+
+            if row + 1 < height && input[value + (width + 1)] != Char::NumberSign {
+                next.insert(value + (width + 1));
+            }
+        }
+
+        visited = next;
+    }
+
+    println!("dimensions: {} x {}", width, height);
+    Ok(visited.len())
+}
+fn part1() -> Result<usize> {
+    let input = include_str!("input.txt");
+
+    let width = input.find('\n').expect("newline");
+    let height = input.len() / (width + 1);
+    let start = input.find('S').expect("start");
+    let input = input.as_ascii().expect("ascii");
+    println!("dimensions: {} x {}", width, height);
 
     let mut visited: HashSet<usize> = HashSet::new();
     visited.insert(start);
@@ -48,8 +98,5 @@ fn main() -> Result<()> {
         visited = next;
     }
 
-    println!("dimensions: {} x {}", width, height);
-    println!("Part 1: {}", visited.len());
-    Ok(())
+    Ok(visited.len())
 }
-
