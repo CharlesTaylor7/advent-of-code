ee28aa3fbf8f2addcfaa1ea22f3963dc35205121
Charles Taylor <charlestaylor95@gmail.com>
2024-03-28 16:35:55 -0400

    Python

---
+++ 2023/21-step-counter/main.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+def main():
+    with open('./2023/21-step-counter/example.txt', 'r') as f:
+        garden: list[str] = f.readlines()
+
+    pass
+    
+    
+
+main()
