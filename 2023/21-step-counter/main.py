936041dd094a100858bfc48cdab6428da294eb51
Charles Taylor <charlestaylor95@gmail.com>
2024-03-30 20:51:31 -0400

    WIP

--- 2023/21-step-counter/main.py
+++ 2023/21-step-counter/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 import sys
 import time
 from typing import Iterator, Literal, Tuple, TypeAlias
@@ -30,7 +31,9 @@ class Garden:
     min_y: int
     max_y: int
     wave: list[Point] = field(default_factory=list)
-    visited: set[Point] = field(default_factory=set)
+    visited: defaultdict[Point, set[Point]] = field(
+        default_factory=lambda: defaultdict(set)
+    )
 
     @classmethod
     def parse(cls, file_path: str) -> "Garden":
@@ -71,7 +74,7 @@ class Garden:
             for x in range(self.min_x, self.max_x + 1):
                 if (x, y) in self.wave:
                     sys.stdout.write("*")
-                elif (x, y) in self.visited:
+                elif (x, y) in self.visited
                     sys.stdout.write("-")
                 elif (x % self.width, y % self.height) in self.rocks:
                     sys.stdout.write("#")
@@ -81,31 +84,6 @@ class Garden:
 
         sys.stdout.flush()
         time.sleep(0.5)
-
-    def part1(self, step_count: int) -> int:
-        self.visited = {self.start}
-        for _ in range(step_count):
-            next = set()
-            for x, y in self.visited:
-                # left
-                if x > 0 and (x - 1, y) not in self.rocks:
-                    next.add((x - 1, y))
-
-                # right
-                if x < self.width - 1 and (x + 1, y) not in self.rocks:
-                    next.add((x + 1, y))
-
-                # above
-                if y > 0 and (x, y - 1) not in self.rocks:
-                    next.add((x, y - 1))
-
-                # below
-                if y < self.height - 1 and (x, y + 1) not in self.rocks:
-                    next.add((x, y + 1))
-
-            self.visited = next
-
-        return len(self.visited)
 
     def neighbors(self) -> Iterator[Point]:
         for x, y in self.wave:
@@ -121,19 +99,27 @@ class Garden:
             # below
             yield (x, y + 1)
 
+    def has_visited(self, point: Point) -> bool: 
+
+
     def part2(self, step_count: int) -> int:
+        self.visited[(0, 0)].add(self.start)
+
         self.wave = [self.start]
-        self.visited = {self.start}
         for step in range(step_count):
-            # self.print(step)
+            self.print(step)
             next = []
             for point in self.neighbors():
-                normalized = (point[0] % self.width, point[1] % self.height)
-                if normalized not in self.rocks and point not in self.visited:
+                (x, y) = point
+                normalized = (x % self.width, y % self.height)
+                key = (x // self.width, y // self.height)
+                if normalized not in self.rocks and normalized not in self.visited[key]:
                     next.append(point)
-                    self.visited.add(point)
+                    sector = self.visited[key]
+                    sector.add(normalized)
+                    if len(sector) + len(self.rocks) == self.width * self.height:
+                        print("cleared sector", key, "at step", step)
 
-                    """
                     if point[0] < self.min_x:
                         self.min_x -= self.width
 
@@ -145,24 +131,23 @@ class Garden:
 
                     if point[1] > self.max_y:
                         self.max_y += self.height
-                    """
 
             self.wave = next
 
+        return 43
+        """
         if (step_count % 2 == 0) ^ is_even(self.start):
-            return sum(1 for point in self.visited if is_odd(point))
+            return sum(1 for key, set in self.visited for point in set if is_odd(point))
         else:
-            return sum(1 for point in self.visited if is_even(point))
+            return sum(
+                1 for key, set in self.visited for point in set if is_even(point)
+            )
+            """
 
 
 def run(file: str, part: Part, step_count: int) -> int:
     garden = Garden.parse(file)
-    return garden.part1(step_count) if part == "part-1" else garden.part2(step_count)
-
-
-def test_part1():
-    assert run("./2023/21-step-counter/example.txt", "part-1", 6) == 16
-    assert run("./2023/21-step-counter/input.txt", "part-1", 64) == 3820
+    return garden.part2(step_count)
 
 
 def test_part2_small():
@@ -181,8 +166,8 @@ def test_part2_large():
 def main():
     # print(run("./2023/21-step-counter/input.txt", "part-2", 26501365))
 
-    print(run("./2023/21-step-counter/example.txt", "part-2", 5000))
-    # print(run("./2023/21-step-counter/example.txt", "part-2", 10))
+    # print(run("./2023/21-step-counter/example.txt", "part-2", 5000))
+    print(run("./2023/21-step-counter/example.txt", "part-2", 10))
 
 
 if __name__ == "__main__":
