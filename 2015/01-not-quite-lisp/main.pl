fa345578e587772ee85a1199af6ab7a8d17d2ea0
Charles Taylor <charlestaylor95@gmail.com>
2026-01-17 12:03:05 -0500

    WIP

--- 2015/01-not-quite-lisp/main.pl
+++ 2015/01-not-quite-lisp/main.pl
@@ -6,13 +6,37 @@ read_input(File, Input) :-
     maplist(char_code, Input, Codes),
     close(Stream).
 
+% Part 1
+santa_part1([], 0).
 
-% ?- input(I), santa_direction(I, T).
+santa_part1([')' | Rest], Floor) :- 
+  santa_part1(Rest, Floor2), 
+  Floor is Floor2 - 1.
 
-santa_direction([],0).
-santa_direction([')' | R], T) :- santa_direction(R, T2), T is T2 - 1.
-santa_direction(['(' | R], T) :- santa_direction(R, T2), T is T2 + 1.
+santa_part1(['(' | Rest], Floor) :- 
+  santa_part1(Rest, Floor2), 
+  Floor is Floor2 + 1.
+
 ?- 
   read_input("input.txt", Input),
-  santa_direction(Input, Tally), 
-  write(Tally).
+  santa_part1(Input, Floor), 
+  write(Floor).
+%
+% % Part 2
+% % the empty list means we traverse no floors
+santa_part2([], _, 0, []) :- !.
+% reaching floor -1, means we have reached our goal:
+santa_part2(_, Index, -1, Index) :- !.
+santa_part2([')' | Rest], Index, Floor, FirstIndex) :- 
+  NextFloor is Floor - 1,
+  NextIndex is Index + 1,
+  santa_part2(Rest, NextIndex, NextFloor, FirstIndex).
+%
+% santa_part2(['(' | Rest], Index, Floor, FirstIndex) :- 
+%   santa_part2(Rest, 1 + Index, Floor2), 
+%   Floor is Floor2 + 1.
+%
+% ?-
+%   read_input("input.txt", Input),
+%   santa_part2(Input, Index, -1), 
+%   write(Index).
