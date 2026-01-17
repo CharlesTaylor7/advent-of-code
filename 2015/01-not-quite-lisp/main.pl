8c6496db31783ccc8b7a893ec4c546271dd021f6
Charles Taylor <charlestaylor95@gmail.com>
2026-01-17 12:00:12 -0500

    swipl 2015 day 1

--- 2015/01-not-quite-lisp/main.pl
+++ 2015/01-not-quite-lisp/main.pl
@@ -3,11 +3,8 @@
 read_input(File, Input) :-
     open(File, read, Stream),
     read_line_to_codes(Stream, Codes),
-    maplist(code_to_atom, Codes, Input),
+    maplist(char_code, Input, Codes),
     close(Stream).
-
-code_to_atom(Code, Atom) :-
-    char_code(Atom, Code).
 
 
 % ?- input(I), santa_direction(I, T).
@@ -15,4 +12,7 @@ code_to_atom(Code, Atom) :-
 santa_direction([],0).
 santa_direction([')' | R], T) :- santa_direction(R, T2), T is T2 - 1.
 santa_direction(['(' | R], T) :- santa_direction(R, T2), T is T2 + 1.
-?- santa_direction([')', '('], T), write(T).
+?- 
+  read_input("input.txt", Input),
+  santa_direction(Input, Tally), 
+  write(Tally).
