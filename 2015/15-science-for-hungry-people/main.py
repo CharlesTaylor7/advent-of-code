bdacc9d430e54cd3556777279c110b262e0aebda
Charles Taylor <charlestaylor95@gmail.com>
2026-03-29 10:22:31 -0400

    python day 15

---
+++ 2015/15-science-for-hungry-people/main.py
@@ -0,0 +1,86 @@
+# // idea is dynamic programming.
+from typing import Tuple
+import re
+from os import path
+REGEX = re.compile(r'\w+: capacity (?P<capacity>-?\d+), durability (?P<durability>-?\d+), flavor (?P<flavor>-?\d+), texture (?P<texture>-?\d+), calories (?P<calories>-?\d+)')
+
+matrix = []
+with open(path.normpath(path.join(__file__, "../input.txt")), 'r') as file:
+    for line in file.readlines():
+        match = re.match(REGEX, line)
+        if (not match):
+            continue
+        
+        matrix.append([int(v) for v in match.groups()])
+
+def add(v1, v2):
+    return (v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2], v1[3] + v2[3], v1[4] + v2[4])
+
+def tastiness(v):
+    total = 1
+    for i in range(4):
+        if v[i] <= 0: 
+            return 0
+        total *= v[i]
+
+    return total
+
+Vector = Tuple[int, int, int, int, int]
+ways: list[set[Vector]] = [set() for _ in range(501)]
+
+ways[0].add((0, 0, 0, 0, 0))
+for i in range(500):
+    for way in ways[i]:
+        for j in range(4):
+            ingredient = matrix[j]
+            cal = way[4] + ingredient[4]
+            if cal > 500:
+                continue
+            ways[cal].add(add(way, ingredient))
+
+print(max(tastiness(way) for way in ways[500]))
+
+
+#   heap.insert(-tastiness(initial as Vector), initial as Vector);
+#   while (true) {
+#     const min = heap.findMin();
+#     if (min == null) throw new Error("heap empty!");
+#     const [_, vector] = min;
+#
+#     for (let i = 0; i < 4; i++) {
+#       const next = Array.from(vector) as Vector;
+#
+#       for (let j = 0; j < 5; j++) {
+#         (next as number[])[j] += matrix[i][j] as number;
+#       }
+#
+#       console.log(next, seen.size);
+#       const s = next.toString();
+#       if (seen.has(s)) {
+#         continue;
+#       } else {
+#         seen.add(s);
+#       }
+#
+#       if (next[0] > 500) continue;
+#       if (next[0] === 500) {
+#         const t = tastiness(next);
+#         if (t > max) max = t;
+#         continue;
+#       }
+#
+#       heap.insert(-tastiness(next), next);
+#     }
+#   }
+#   console.log(matrix);
+# }
+#
+# function tastiness(vector: Vector) {
+#   let total = 1;
+#   for (let n = 1; n < vector.length; n++) {
+#     if (vector[n] <= 0) return 0;
+#     total *= vector[n];
+#   }
+#   return total;
+# }
+# console.l
--- 2015/15-science-for-hungry-people/main.ts
+++ 2015/15-science-for-hungry-people/main.ts
@@ -1,15 +1,14 @@
-import { FibHeap } from "@/queue.ts";
 const REGEX =
   /\w+: capacity (?<capacity>-?\d+), durability (?<durability>-?\d+), flavor (?<flavor>-?\d+), texture (?<texture>-?\d+), calories (?<calories>-?\d+)/;
 
 const DIMENSIONS = ["calories", "capacity", "durability", "flavor", "texture"];
 type Branded<T> = number & { __brand: T };
 type Dim = typeof DIMENSIONS[number];
-type Calories = Branded<"calories">;
-type Capacity = Branded<"capacity">;
-type Durability = Branded<"durability">;
-type Flavor = Branded<"flavor">;
-type Texture = Branded<"texture">;
+type Calories = number;
+type Capacity = number;
+type Durability = number;
+type Flavor = number;
+type Texture = number;
 
 type Vector = [Calories, Capacity, Durability, Flavor, Texture];
 
@@ -39,15 +38,12 @@ function part2() {
       brand(groups, "texture"),
     ]);
   }
+  console.log(matrix);
+  return;
 
-  const heap = new FibHeap<Vector>();
-  let initial = [0, 0, 0, 0, 0];
-
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 5; j++) {
-      initial[j] += matrix[i][j] as number;
-    }
-  }
+  // calories -> List of vectors that hit that calorie count
+  const ways: Array<Set<string>> = Array(500);
+  ways.push([[0, 0, 0, 0, 0]]);
 
   let max = Number.NEGATIVE_INFINITY;
   const seen = new Set<string>();
@@ -95,75 +91,3 @@ function tastiness(vector: Vector) {
   return total;
 }
 console.log(part2());
-//
-//
-// # dumb but works
-// # just start at an arbitrary point walk towards a local maximum
-// export def part1 [] {
-//   let costs = $in | parse -r $regex
-//   mut partition = [25 25 25 25]
-//   mut max = eval $costs $partition
-//   mut continue = true
-//   while $continue {
-//     $continue = false
-//     for i in (seq 0 3) {
-//       for j in (seq 0 3 | filter { $in != $i }) {
-//         let p = $partition | update $i { $in + 1 } | update $j { $in - 1 }
-//         let score = taste $costs $p
-//         if $score > $max {
-//           $max = $score
-//           $partition = $p
-//           $continue = true
-//         }
-//       }
-//     }
-//   }
-//   $max
-// }
-//
-// export def part2 [] {
-//
-// }
-//
-// export def calories [costs: table, amounts: list<int>] {
-//   $costs | get calories | into int | zip $amounts | each { |pair| $pair.0 * $pair.1 } | math sum
-// }
-//
-// export def taste [costs: table, amounts: list<int>] {
-//   let a =  $costs | get capacity | into int | zip $amounts | each { |pair| $pair.0 * $pair.1 } | math sum
-//   let b = $costs | get durability | into int | zip $amounts | each { |pair| $pair.0 * $pair.1 } | math sum
-//   let c = $costs | get flavor | into int | zip $amounts | each { |pair| $pair.0 * $pair.1 } | math sum
-//   let d = $costs | get texture | into int | zip $amounts | each { |pair| $pair.0 * $pair.1 } | math sum
-//   if $a <= 0 or $b <= 0 or $c <= 0 or $d <= 0 {
-//     0
-//   } else {
-//     $a * $b * $c * $d
-//   }
-// }
-//
-//
-//
-// def count [buckets: list<int>, calories: int] {
-//   stor reset
-//   stor create -t calories -c { i: int, q: int, n: int }
-//   stor open | query db "create index cache on calories(i, q)"
-//
-//   let n = ($buckets | length) - 1
-//   seq 1 $calories
-//   | each { go $buckets $n $in }
-//   | last
-// }
-//
-// # i is the index of the largest available bucket
-// def go [buckets: list<int>, i: int, q: int] {
-//   if $q == 0 { return 1 }
-//   if $i < 0 { return 0 }
-//   if $q < 0 { return 0 }
-//   let rows = stor open | query db $"select n from calories where i = ($i) and q = ($q)"
-//   if ($rows | is-not-empty) { return $rows.0.n }
-//
-//   let k = $buckets | get $i
-//   let n = (go $buckets ($i - 1) $q) + (go $buckets ($i - 1) ($q - $k))
-//   stor insert -t eggnog -d { i: $i, q: $q, n: $n }
-//   $n
-// }
