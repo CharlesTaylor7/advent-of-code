ecfd7f40a8a977a122b3559a64d430bf500edd7a
Charles Taylor <charlestaylor95@gmail.com>
2026-06-10 10:20:44 -0400

    Update zig template

--- templates/zig/main.zig
+++ templates/zig/main.zig
@@ -1,49 +1,46 @@
 const std = @import("std");
-const AocError = error{ NotImplemented, InvalidPart, MissingArg };
+const AocError = error{ NotImplemented, InvalidPart, MissingArg, InvalidRange };
 const Part = enum { one, two };
 const Args = struct { part: Part, filename: []const u8 };
 
 pub fn main(init: std.process.Init) !void {
     const args = try parse_args(init.minimal.args);
 
-    const cwd = std.Io.Dir.cwd();
+    const file = try std.Io.Dir.cwd().openFile(init.io, args.filename, .{});
+    const answer = try solve(init, file, args.part);
+    const buffer = try init.gpa.alloc(u8, 1024);
+    defer init.gpa.free(buffer);
 
-    const fileContents = try cwd.readFileAlloc(init.io, args.filename, init.gpa, .unlimited);
-    defer init.gpa.free(fileContents);
-
-    const answer = try switch (args.part) {
-        Part.one => part1(fileContents),
-        Part.two => part2(fileContents),
-    };
-    std.debug.print("{d}\n", .{answer});
+    var stdout = std.Io.File.stdout().writer(init.io, buffer);
+    try stdout.interface.print("{d}\n", .{answer});
+    try stdout.interface.flush();
 }
 
-fn parse_args(passedArgs: std.process.Args) AocError!Args {
+fn parse_args(passedArgs: std.process.Args) !Args {
     var args = passedArgs.iterate();
     _ = args.skip();
-    const filename = args.next() orelse return error.MissingArg;
-    const rawPart = args.next() orelse return error.MissingArg;
+    const filename = args.next() orelse return AocError.MissingArg;
+    const rawPart = args.next() orelse return AocError.MissingArg;
     const part = try parse_part(rawPart);
     return .{ .part = part, .filename = filename };
 }
 
-fn parse_part(arg: []const u8) AocError!Part {
-    const part = std.fmt.parseInt(u8, arg, 10) catch return AocError.InvalidPart;
+fn parse_part(arg: []const u8) !Part {
+    const part = try std.fmt.parseInt(u8, arg, 10);
     if (part == 1) return Part.one;
     if (part == 2) return Part.two;
     return AocError.InvalidPart;
 }
 
-pub fn part1(fileContents: []const u8) !u64 {
-    var rows = std.mem.splitScalar(u8, fileContents, '\n');
-    while (rows.next()) |row| {
-        _ = row;
+fn solve(init: std.process.Init, file: std.Io.File, part: Part) !u64 {
+    _ = part;
+
+    const buffer = try init.gpa.alloc(u8, 1024);
+    defer init.gpa.free(buffer);
+
+    var reader = file.reader(init.io, buffer);
+    while (try reader.interface.takeDelimiter('\n')) |line| {
+        _ = line;
     }
-
-    return AocError.NotImplemented;
-}
-
-pub fn part2(fileContents: []const u8) !u64 {
-    _ = fileContents;
-    return AocError.NotImplemented;
+    return 42;
 }
