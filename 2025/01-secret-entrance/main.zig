64d27db73625736e8c969c52d2cfaad7ed34ecba
Charles Taylor <charlestaylor95@gmail.com>
2026-06-10 11:03:39 -0400

    Redo 1 with zig 16

--- 2025/01-secret-entrance/main.zig
+++ 2025/01-secret-entrance/main.zig
@@ -1,99 +1,90 @@
 const std = @import("std");
+const AocError = error{ NotImplemented, InvalidPart, MissingArg, InvalidRange };
+const Part = enum { one, two };
+const Args = struct { part: Part, filename: []const u8 };
 
-pub fn main() !void {
-    // Initiate allocator
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const alloc = gpa.allocator();
+pub fn main(init: std.process.Init) !void {
+    const args = try parse_args(init.minimal.args);
 
-    // Read contents from file "./filename"
-    const cwd = std.fs.cwd();
-    const fileContents = try cwd.readFileAlloc(alloc, "input.txt", 100000);
-    defer alloc.free(fileContents);
+    const file = try std.Io.Dir.cwd().openFile(init.io, args.filename, .{});
+    const answer = try solve(init, file, args.part);
+    const buffer = try init.gpa.alloc(u8, 1024);
+    defer init.gpa.free(buffer);
 
-    const n = try part2(fileContents);
-    std.debug.print("count: {d}\n", .{n});
+    var stdout = std.Io.File.stdout().writer(init.io, buffer);
+    try stdout.interface.print("{d}\n", .{answer});
+    try stdout.interface.flush();
 }
 
-pub fn part1(fileContents: []u8) !u8 {
-    var rows = std.mem.splitScalar(u8, fileContents, '\n');
+fn parse_args(passedArgs: std.process.Args) !Args {
+    var args = passedArgs.iterate();
+    _ = args.skip();
+    const filename = args.next() orelse return AocError.MissingArg;
+    const rawPart = args.next() orelse return AocError.MissingArg;
+    const part = try parse_part(rawPart);
+    return .{ .part = part, .filename = filename };
+}
+
+fn parse_part(arg: []const u8) !Part {
+    const part = try std.fmt.parseInt(u8, arg, 10);
+    if (part == 1) return Part.one;
+    if (part == 2) return Part.two;
+    return AocError.InvalidPart;
+}
+
+fn solve(init: std.process.Init, file: std.Io.File, part: Part) !u64 {
+    const buffer = try init.gpa.alloc(u8, 1024);
+    defer init.gpa.free(buffer);
+
+    var reader = file.reader(init.io, buffer);
+
     const size = i32;
     var door: size = 50;
-    var count: usize = 0;
-    while (rows.next()) |line| {
+    var countPart1: usize = 0;
+    var countPart2: usize = 0;
+    while (try reader.interface.takeDelimiter('\n')) |line| {
         if (line.len == 0) break;
-        if (line[0] == 'L') {
 
-            const n = std.fmt.parseInt(size, line[1..], 10) catch |err| {
-
-                std.debug.print("{s}\n", .{line});
-                return err;
-            };
-
-            std.debug.print("left {d}\n", .{n});
-            door -= n;
-            while (door < 0) { door += 100; }
-        }
-
-        if (line[0] == 'R') {
-            const n = try std.fmt.parseInt(size, line[1..], 10);
-            std.debug.print("right {d}\n", .{n});
-
-            door += n;
-            while (door >= 100) { door -= 100; }
-        }
-
-
-        std.debug.print("door: {d}\n", .{door});
-        if (door == 0) { count += 1; }
-
-    }
-    return count;
-}
-const InvalidFileError = error{ BadDirection };
-
-pub fn part2(fileContents: []u8) !usize {
-    var rows = std.mem.splitScalar(u8, fileContents, '\n');
-    const size = i32;
-    var door: size = 50;
-    var count: usize = 0;
-
-    std.debug.print("door: {d}\n", .{door});
-    while (rows.next()) |line| {
         if (line.len == 0) break;
 
         std.debug.print("{s}\n", .{line});
-        const n = try std.fmt.parseInt(size, line[1..], 10) ;
-        const sign: size = if (line[0] == 'L' )  -1 else if (line[0] == 'R')  1  else return InvalidFileError.BadDirection;
+        const n = try std.fmt.parseInt(size, line[1..], 10);
+        const sign: size = if (line[0] == 'L') -1 else if (line[0] == 'R') 1 else return InvalidFileError.BadDirection;
 
         const startedAtZero = door == 0;
         if (sign == -1) {
             door -= n;
             while (door < 0) {
                 door += 100;
-                count += 1;
+                countPart2 += 1;
                 std.debug.print("click\n", .{});
             }
             if (door == 0) {
-                count += 1;
+                countPart2 += 1;
                 std.debug.print("click\n", .{});
             }
             if (startedAtZero) {
-                count -= 1;
+                countPart2 -= 1;
                 std.debug.print("unclick\n", .{});
             }
         }
+
         if (sign == 1) {
             door += n;
             while (door >= 100) {
                 door -= 100;
-                count += 1;
+                countPart2 += 1;
                 std.debug.print("click\n", .{});
             }
         }
-
         std.debug.print("door: {d}\n", .{door});
-
+        if (door == 0) {
+            countPart1 += 1;
+        }
     }
-    return count;
+    return switch (part) {
+        .one => countPart1,
+        .two => countPart2,
+    };
 }
+const InvalidFileError = error{BadDirection};
