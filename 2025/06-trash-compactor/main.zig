8168e0074c168accbc02308ab88fc06a1c77ea06
Charles Taylor <charlestaylor95@gmail.com>
2026-06-11 13:21:03 -0400

    Use packed union to compress for cephalod math

--- 2025/06-trash-compactor/main.zig
+++ 2025/06-trash-compactor/main.zig
@@ -4,8 +4,7 @@ const Part = enum { one, two };
 const Args = struct { part: Part, filename: []const u8 };
 
 pub fn main(init: std.process.Init) !void {
-    std.debug.print("{d}\n", .{@bitSizeOf(SlimFileChar)});
-    std.debug.print("{d}\n", .{@bitSizeOf(FileCharStruct)});
+    std.debug.print("{d}\n", .{@bitSizeOf(FileChar)});
     const args = try parse_args(init.minimal.args);
 
     const file = try std.Io.Dir.cwd().openFile(init.io, args.filename, .{});
@@ -39,79 +38,85 @@ fn parse_part(arg: []const u8) !Part {
 
 const Section = enum { digits, operators };
 
-const FileCharEnum = enum(u2) { digit, space, op };
-const Operator = enum(u1) { plus, times };
 // 10 digits + 1 space + 2 operators == 13 options.
 // which can be encoded in 4 bits. half a byte;
 // space == 10,
 // plus = 11,
 // times =12```
-const SlimFileChar = enum { space, zero, one, two, three, four, five, six, seven, eight, nine, plus, minus };
+const Space = enum(u4) { space = 10 };
+const Operator = enum(u4) { plus = 11, times = 12 };
+const FileCharTag = enum(u2) { digit, space, op };
+const FileCharUnion = packed union { digit: u4, space: Space, op: Operator };
+// would work but is oh so boring
+// const SlimFileChar = enum { space, zero, one, two, three, four, five, six, seven, eight, nine, plus, minus };
 
-const FileCharStruct = packed struct {
-    val: u4,
+const FileChar = packed struct {
+    val: FileCharUnion,
 
-    fn match(self: FileCharStruct, comptime handleDigit: anytype, comptime handleSpace: anytype, comptime handleOp: anytype) @typeInfo(handleDigit).Fn.return_type.? {
-        const val = self.val;
-        if (val < 10) return handleDigit(val);
-        if (val == 10) return handleSpace();
-        if (val == 11) return handleOp(.plus);
-        if (val == 12) return handleOp(.times);
-        unreachable;
+    fn of(val: FileCharUnion) FileChar {
+        return FileChar{ .val = val };
+    }
+    fn tag(self: FileChar) FileCharTag {
+        if (self.val.digit < 10) return FileCharTag.digit;
+        if (self.val.digit == 10) return FileCharTag.space;
+        return FileCharTag.op;
     }
 };
+const FileContents = struct {
+    chars: []FileChar,
+    rows: usize,
+    cols: usize,
+};
+
+fn parse_file(init: std.process.Init, file: std.Io.File) !FileContents {
+    defer file.close(init.io);
+
+    var total: u64 = 0;
+    total += 0;
+
+    var cols: usize = 0;
+    var rows: usize = 0;
+    var index: usize = 0;
+
+    const stat = try file.stat(init.io);
+    const fileChars: []FileChar = try init.gpa.alloc(FileChar, stat.size);
+
+    const buffer = try init.gpa.alloc(u8, 4096);
+    defer init.gpa.free(buffer);
+
+    var reader = file.reader(init.io, buffer);
+
+    while (try reader.interface.takeDelimiter('\n')) |line| {
+        if (cols == 0) {
+            cols = line.len;
+        } else if (line.len != cols) {
+            return error.UnevenLines;
+        }
+        for (line) |c| {
+            fileChars[index] = switch (c) {
+                '0'...'9' => FileChar.of(.{ .digit = @intCast(c - 48) }),
+                ' ' => FileChar.of(.{ .space = .space }),
+                '+' => FileChar.of(.{ .op = .plus }),
+                '*' => FileChar.of(.{ .op = .times }),
+                else => return error.Invalid,
+            };
+            index += 1;
+        }
+        rows += 1;
+    }
+
+    return FileContents{
+        .rows = rows,
+        .cols = cols,
+        .chars = fileChars,
+    };
+}
+
 // idea is to load everything into memory and assert lines are even in length.
 // then process the file column by column
 fn part2(init: std.process.Init, file: std.Io.File) !u64 {
-    var total: u64 = 0;
-    total += 0;
-    var section = Section.digits;
-
-    var fileContents: []u8 = undefined;
-    defer init.gpa.free(fileContents);
-    {
-        // goal of this block is to read the entire file into a buffer and allocate. also closes the file,
-        defer file.close(init.io);
-        const stat = try file.stat(init.io);
-        fileContents = try init.gpa.alloc(u8, stat.size);
-
-        var reader = file.reader(init.io, fileContents);
-        var cols: usize = 0;
-        var rows: usize = 0;
-        var firstLine = true;
-
-        while (try reader.interface.takeDelimiter('\n')) |line| {
-            if (line[0] == '*' or line[0] == '+') {
-                section = Section.operators;
-            }
-
-            switch (section) {
-                .digits => {
-                    rows += 1;
-                    var iter = std.mem.splitScalar(u8, line, ' ');
-                    while (iter.next()) |num| {
-                        if (num.len == 0) continue;
-                        if (firstLine) cols += 1;
-                    }
-                    firstLine = false;
-                },
-                .operators => {
-                    var i: usize = 0;
-                    var iter = std.mem.splitScalar(u8, line, ' ');
-                    while (iter.next()) |op| {
-                        if (op.len == 0) continue;
-                        switch (op[0]) {
-                            '+' => {},
-                            '*' => {},
-                            else => return error.InvalidOperator,
-                        }
-                        i += 1;
-                    }
-                },
-            }
-        }
-    }
-    return total;
+    _ = try parse_file(init, file);
+    return 2;
 }
 fn part1(init: std.process.Init, file: std.Io.File) !u64 {
     var total: u64 = 0;
