13b0a62910296209818f189ca3b8cffe95c4622d
Charles Taylor <charlestaylor95@gmail.com>
2026-06-19 10:30:20 -0400

    Save more puzzles

---
+++ 2025/12-christmas-tree-farm/main.zig
@@ -0,0 +1,68 @@
+const std = @import("std");
+const AocError = error{ NotImplemented, InvalidPart, MissingArg, InvalidRange };
+const Part = enum { one, two };
+const Args = struct { part: Part, filename: []const u8 };
+
+pub fn main(init: std.process.Init) !void {
+    const args = try parse_args(init.minimal.args);
+
+    const file = try std.Io.Dir.cwd().openFile(init.io, args.filename, .{});
+    const answer = try solve(init, file, args.part);
+    const buffer = try init.gpa.alloc(u8, 1024);
+    defer init.gpa.free(buffer);
+
+    var stdout = std.Io.File.stdout().writer(init.io, buffer);
+    try stdout.interface.print("{d}\n", .{answer});
+    try stdout.interface.flush();
+}
+
+fn parse_args(passedArgs: std.process.Args) !Args {
+    var args = passedArgs.iterate();
+    _ = args.skip();
+    const filename = args.next() orelse return AocError.MissingArg;
+    const rawPart = args.next() orelse return AocError.MissingArg;
+    const part = try parse_part(rawPart);
+    return .{ .part = part, .filename = filename };
+}
+
+fn parse_part(arg: []const u8) !Part {
+    const part = try std.fmt.parseInt(u8, arg, 10);
+    if (part == 1) return Part.one;
+    if (part == 2) return Part.two;
+    return AocError.InvalidPart;
+}
+
+fn solve(init: std.process.Init, file: std.Io.File, part: Part) !u64 {
+    return switch (part) {
+        .one => part1(init, file),
+        .two => part2(init, file),
+    };
+}
+
+fn part1(init: std.process.Init, file: std.Io.File) !u64 {
+    {
+        const buffer = try init.gpa.alloc(u8, 1024);
+        defer init.gpa.free(buffer);
+        defer file.close(init.io);
+
+        var reader = file.reader(init.io, buffer);
+        while (try reader.interface.takeDelimiter('\n')) |line| {
+            _ = line;
+        }
+    }
+    return 42;
+}
+
+fn part2(init: std.process.Init, file: std.Io.File) !u64 {
+    {
+        const buffer = try init.gpa.alloc(u8, 1024);
+        defer init.gpa.free(buffer);
+        defer file.close(init.io);
+
+        var reader = file.reader(init.io, buffer);
+        while (try reader.interface.takeDelimiter('\n')) |line| {
+            _ = line;
+        }
+    }
+    return 42;
+}
